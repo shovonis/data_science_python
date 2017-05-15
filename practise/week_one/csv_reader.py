2c693587b41b278e02c96869ea92d74f4156ab96
Rifatul Islam Shovon <rifatul.islam@therapservices.net>
2017-05-15 13:53:03 +0600

    Git ignore file added

--- practise/week_one/csv_reader.py
+++ practise/week_one/csv_reader.py
@@ -6,17 +6,17 @@ _author_ = "rifatul.islam"
 with open("mpg.csv") as csv_file:
     mpg = list(csv.DictReader(csv_file))
 
-print(mpg[:3])
+print(mpg[:10])
 print(len(mpg))
 print(mpg[0].keys())
 print(sum(float(d['weight']) for d in mpg) / len(mpg))
 
+# Avg mpg per cylinder
+
 cyl = set(d['cylinders'] for d in mpg)
 print(cyl)
-
-
-# Avg mpg per cylinder
 avg_mpg_cyl = []
+
 for c in cyl:
     sum_mpg = 0
     total_mpg = 0
@@ -25,8 +25,25 @@ for c in cyl:
             sum_mpg += float(d['mpg'])
             total_mpg += 1
 
-    avg_mpg_cyl.append((c, sum_mpg/total_mpg))
+    avg_mpg_cyl.append((c, sum_mpg / total_mpg))
 
 avg_mpg_cyl.sort(key=lambda x: x[0])
 print(avg_mpg_cyl)
 
+# Average acceleration per origin class
+origin_class = set(d['origin'] for d in mpg)
+acceleration = []
+
+for oc in origin_class:
+    sum_hp = 0
+    counter = 0
+    for d in mpg:
+        if oc == d['origin']:
+            sum_hp += float(d['acceleration'])
+            counter += 1
+
+    acceleration.append((oc, sum_hp / counter))
+
+acceleration.sort(key=lambda x: x[0])
+
+print("Avg acceleration: ", acceleration)
