0de6a2768b80bee61040246fa876027f244b76ac
Rifatul Islam Shovon <rifatul.islam@therapservices.net>
2017-05-21 14:31:29 +0600

    Comprehensive list

--- practise/week_one/date_time.py
+++ practise/week_one/date_time.py
@@ -11,5 +11,26 @@ print(today.year, today.day, today.month)
 
 time_delta = datetime.timedelta(days=30)
 
-print(today-time_delta)
+print(today - time_delta)
 print(today > today - time_delta)
+
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+
+def split_title_and_name(person):
+    return person.split()[0] + ' ' + person.split()[-1]
+
+
+# option 1
+for person in people:
+    print(split_title_and_name(person) == (lambda x: x.split()[0] + ' ' + x.split()[-1])(person))
+
+# option 2
+var = list(map(split_title_and_name, people)) == list(
+    map(lambda person: person.split()[0] + ' ' + person.split()[-1], people))
+
+
+lowercase = 'abcdefghijklmnopqrstuvwxyz'
+digits = '0123456789'
+
+answer = [x+y+i+j for x in lowercase for y in lowercase for i in digits for j in digits]
